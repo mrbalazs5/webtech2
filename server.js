0492bfa4cbb46e966f16c6351a57c5cab8b44129
Bazsi <megbalaa@gmail.com>
2020-03-18 14:29:14 +0100

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.get('/test', (req, res) => {
+    res.json({test: 'test'});
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
