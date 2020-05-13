bdca347f993bc5ee4566cd9281fcbb9cdf2c19ff
pankotaimarksandor <pankotaimarksandor@gmail.com>
2020-05-13 15:23:16 +0200

    table update

--- client/src/components/ModelsTable.js
+++ client/src/components/ModelsTable.js
@@ -3,34 +3,37 @@ import './Tables.scss';
 
 function ModelTable (props) {
   return(
-    <div>
+    <div className={'table'}>
       {
         props.models.map((model, id) => {
           return(
-            <div key={id}>
-              <div>{model.make.name}</div>
-              <div>{model.name}</div>
-              {
-                model.generations.map((generation, id) => {
-                  return(
-                    <div key={id}>
-                      <div>{generation.name}</div>
-                      <div>{generation.yearBegin}</div>
-                      <div>{generation.yearEnd}</div>
-                      {
-                        generation.series.map((seria, id) => {
-                          return(
-                            <div key={id}>
-                              <div>{seria.name}</div>
-                              
-                            </div>
-                          );
-                        })
-                      }
-                    </div>
-                  );
-                })
-              }
+            <div key={id} className={'row'}>
+              <div className={'cell'}><div className={'item'}>{model.make.name}</div></div>
+              <div className={'cell'}><div className={'item'}>{model.name}</div></div>
+              <div className={'cell'}>
+                {
+                  model.generations.map((generation, id) => {
+                    return(
+                      <div key={id} className={'cell-row'}>
+                        <div className={'item'}>{generation.name}</div>
+                        <div className={'item'}>{generation.yearBegin}</div>
+                        <div className={'item'}>{generation.yearEnd}</div>
+                        <div className={'item'}>
+                          {
+                            generation.series.map((seria, id) => {
+                              return(
+                                <div key={id} className={'cell-cell-row'}>
+                                  <div className={'item'}>{seria.name}</div>
+                                </div>
+                              );
+                            })
+                          }
+                        </div>
+                      </div>
+                    );
+                  })
+                }
+              </div>
             </div>
           );
         })
